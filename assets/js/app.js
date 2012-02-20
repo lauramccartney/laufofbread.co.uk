31e14ef02e107f452550f17fd859b4c97b4ac678
James Aitken <loonypandora@gmail.com>
2012-02-20 07:05:45 +0000

    Update animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@ var currentSlide = 0;
 // JS for the site. Not minified so it can be used as a learning tool
 $(document).ready(function () {
 
+    gotoHash();
     addFirstSlide();
 
     $(document).bind('keyup', 'left', function(){
@@ -15,22 +16,32 @@ $(document).ready(function () {
         $('#next').click();
     });
 
-    $('#prev').click(function() {
+    $('#prev').click(function(event) {
         event.preventDefault(); 
+
+        var prevSlide = currentSlide - 1;
 
         if (currentSlide == 0) {
             return;
+        }
+
+        // Add the previous slide, if we've landed in the middle of the deck
+        if ($('#' + portfolio[prevSlide].id).length == 0) {            
+            $('<li/>', {
+                'id': portfolio[prevSlide].id,
+                'class': 'animated',
+            }).css('z-index', prevSlide + 10).appendTo('.slides');
         }
 
         $('#' + portfolio[currentSlide].id)
             .removeClass()
             .addClass('animated slideOut');
 
-        currentSlide--;
+        currentSlide = prevSlide;
 
         updateHash();
     });
-    $('#next').click(function() {
+    $('#next').click(function(event) {
         event.preventDefault();
 
         var nextSlide = currentSlide + 1;
@@ -55,10 +66,15 @@ $(document).ready(function () {
 
         updateHash();
     });
-
-    var hash = window.location.hash.replace( /^#\//, '' );
+    
 });
 
+
+function gotoHash() {
+    var hash = window.location.hash.replace( /^#!\//, '' );
+
+    currentSlide = portfolio_index[hash] || 0;
+}
 
 
 function updateHash() {
@@ -68,7 +84,6 @@ function updateHash() {
 
 function addFirstSlide() {
     $('<li/>', {
-        'id': portfolio[0].id
-    }).css('z-index', 10).prependTo('.slides');            
+        'id': portfolio[currentSlide].id
+    }).css('z-index', currentSlide + 10).prependTo('.slides');            
 }
-
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -9,3 +9,9 @@ var portfolio = [
     }{% if forloop.last %}{% else %},{% endif %}
     {% endfor %}
 ]
+
+
+var portfolio_index = {
+    {% for post in site.categories.portfolio  %}"{{ post.id | remove_first:'/' | replace:'/','-' | escape }}" : {{ forloop.index0 }}{% if forloop.last %}{% else %},{% endif %}
+    {% endfor %}
+}
