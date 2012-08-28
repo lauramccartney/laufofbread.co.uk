784545dafb788f923d88433c03d6877b856039b6
James Aitken <loonypandora@gmail.com>
2012-08-28 23:11:48 +0100

    Add retina capability, and some other stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,56 @@
 
 $(function() {
-    $('.images li').click('click', function(){
+    
+    // If the images are there, swap them out for their retina counterparts
+    $('img').retina({
+        checkIfImageExists: true,
+        suffix: "@2x"
+    });
 
-        var img = $(this).children('img');
-        var ul  = $(this).parent('ul');
 
+    // Add the thumbnail attributes to all images before we go anywhere
+    $('.images img').each(function() {
+        $(this).data('thumbnail', $(this).attr('src'));
+    })
+
+
+    // Every time we click an image, fire this function
+    $('.images li').click(function(){        
+
+        // Sets all images on the page to the thumbnail.
+        $('.images img').each(function() {
+            $(this).attr('src', $(this).data('thumbnail')); 
+        });
+
+        // If we've clicked on the focused image, go back to the main layout
         if ($(this).hasClass('focused')) {
-            ul.removeClass('gallery').addClass('blocks');
+            $(this).parent('ul')
+                   .removeClass('gallery')
+                   .addClass('blocks');   
 
+            // We are going back the main layout, so remove ALL classes       
             $('.images li').removeClass('focused unfocused');
+
         } else {
-            $('.images li').removeClass('focused').addClass('unfocused');
+            // Sets the image we clicked on to have the fullsize image
+            $(this).children('img')
+                   .addClass('focused')
+                   .removeClass('unfocused')
+                   .attr('src', $(this).children('img').data('fullsize'));             
 
-            ul.removeClass('blocks').addClass('gallery');
+            // Make sure we are the in the gallery mode
+            $(this).parent('ul')
+                   .removeClass('blocks')
+                   .addClass('gallery');
 
-            $(this).addClass('focused').removeClass('unfocused');
+            // Add the unfocused link to ALL images...
+            $('.images li').removeClass('focused')
+                           .addClass('unfocused');
 
-            // Save the thumbnail to a data-thumbnail attr
-            img.data('thumbnail', img.attr('src'));
-            
-            // Swap out for the fullsize image
-            img.attr('src', img.data('fullsize'));
+            // Now add the focused class on this link and clear off the unfocused we just added
+            $(this).addClass('focused')
+                   .removeClass('unfocused');
+
         }
     });
 });
